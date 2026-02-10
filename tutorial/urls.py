8710369975009b078716c8519591de9bcb7ada2d
Кира <ivanova.kiru2018@yandex.ru>
2026-02-10 11:43:05 +0700

    5 commit

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from snippets import views
 
 urlpatterns = [
 path('admin/', admin.site.urls),
+   path('api/', include('snippets.urls')),
    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
 ]
